14a531e2a1a7958ed95e2013754ae8c1c22fae0a
zhaotingting <zhaotingting@oaserver.dw.gdbbk.com>
2017-06-19 14:57:10 +0800

    三方应用启动专项

--- module/main.sh
+++ module/main.sh
@@ -12,8 +12,8 @@ echo $1 >> ${exe_log}
 case $1 in
 	"start")
 		echo $$ > ${workdir}/pid
-		pm install -r ${workdir}/performance-test-debug-androidTest.apk
-		pm install -r ${workdir}/performance-test-debug.apk
+		#pm install -r ${workdir}/performance-test-debug-androidTest.apk
+		#pm install -r ${workdir}/performance-test-debug.apk
 		while read line
 		do
 			params=(${line})
@@ -31,18 +31,18 @@ case $1 in
 			top -m 5 >/sdcard/top.txt &
 			tpid=$!
 			#logcat -s ActivityManager:I l:s | grep "${source_package}" --line-buffered > /sdcard/log.txt &
-			logcat -s WindowManager:V ActivityManager:I l:s | grep "${source_package}" --line-buffered > /sdcard/log.txt &
+			logcat -s ActivityManager:I l:s | grep "${source_package}" --line-buffered > /sdcard/log.txt &
 			lpid=$!
 			echo ${pid} ${lpid} > ${workdir}/pid
 			mkdir -p ${workout}/${test_number}
-			am instrument -w -r  -e number ${test_number} -e sysnum ${sys_num} -e type ${test_type} -e appnum ${app_num} -e class ${test_package}.${test_case}\#${test_method} -e count ${test_count} com.eebbk.test.performance.test/android.support.test.runner.AndroidJUnitRunner>${workout}/${test_number}/instrument.txt
+			am instrument -w -r  -e number ${test_number} -e mpackage ${source_package} -e type ${test_type} -e appnum ${app_num} -e class ${test_package}.${test_case}\#${test_method} -e count ${test_count} com.eebbk.test.performance.test/android.support.test.runner.AndroidJUnitRunner>${workout}/${test_number}/instrument.txt
 			kill -9 ${lpid}
 			kill -9 ${tpid}
 			cp -f ${datadir}/${test_number}/* ${workout}/${test_number}
 			cp -f /sdcard/log.txt ${workout}/${test_number}
 			cp -f /sdcard/top.txt ${workout}/${test_number}
 			sleep 5
-		done < ${workdir}/module.txt
+		done < ${workdir}/thirdapp.txt
 		echo "done end ...." >> ${exe_log}
 		echo "done" >> ${trackfile}
 		;;

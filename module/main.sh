b55995b649321f5ae7ea7f82bb5775626ac53ad1
zhaotingting <zhaotingting@oaserver.dw.gdbbk.com>
2017-07-04 16:31:14 +0800

    生成全部测试用例执行文件,执行验证(一)

--- module/main.sh
+++ module/main.sh
@@ -43,7 +43,7 @@ case $1 in
 			#cp -f /sdcard/log.txt ${workout}/${test_number}
 			#cp -f /sdcard/top.txt ${workout}/${test_number}
 			sleep 5
-		done < ${workdir}/module.txt
+		done < ${workdir}/all.txt
 		echo "done" >> ${trackfile}
 		;;
 	"stop")
--- start.py
+++ start.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import testplatform as platform
+import os
 import sys
-from PyQt4.QtGui import QApplication, QStyleFactory
+
+from PyQt4.QtGui import QApplication
+
+import testplatform as platform
 
 
 def main():
@@ -15,3 +18,19 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # mfile = r'D:\bbk-test-center\EebbkTestCenter\performance-test\testcase.ini'
+    # if os.path.exists(mfile):
+    #     with open(r'D:\pendant.txt','wb') as rf:
+    #         with open(mfile, 'rb') as f:
+    #             for line in f:
+    #                 if line.startswith('#'):
+    #                     continue
+    #                 line = line.strip('')
+    #                 list = line.split(' ')
+    #                 clsname = list[0]
+    #                 metname = list[1]
+    #                 pkg = list[2]
+    #                 label = list[3]
+    #                 nlist = ['com.eebbk.test.performance', clsname, metname, metname, 5, pkg, 0, 0, 0]
+    #                 rf.write(" ".join(str(i) for i in nlist))
+    #                 rf.write("\n")
